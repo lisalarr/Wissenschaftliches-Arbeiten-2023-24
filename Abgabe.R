8354177fe30f4bb7ddf09cfa6daf94763914fa0b
lisalarr <88594543+Lisa240801@users.noreply.github.com>
2024-01-02 22:40:39 +0100

    Update Abgabe.R

--- Abgabe.R
+++ Abgabe.R
@@ -8,9 +8,26 @@ titanic = read.csv("~/TU Dortmund/3. Semester/Wissenschaftliches Arbeiten/Gruppe
 # Aufgabe 1 
 
 # Anreden extrahieren
+titanic$Anrede = gsub("^.*,\\s*([A-Za-z]+)\\..*$", "\\1", titanic$Name)
+
 # „Survived“, „Sex“, „Embarked“ zu factor umkodieren
+titanic$Survived = as.factor(titanic$Survived)
+titanic$Sex = as.factor(titanic$Sex)
+titanic$Embarked = as.factor(titanic$Embarked)
+
 # „Pclass“ als ordered-factor
-# NA's von „Age“ imputieren (z.B. arithmetisches Mittel, Median, usw.)
+titanic$Pclass = ordered(as.factor(titanic$Pclass))
+
+# NA's von „Age“ imputieren (Verfahren: arithmetisches Mittel)
+# Gruppierung nach Anrede und Berechnung vom mean
+imputeAge = tapply(titanic$Age, titanic$Anrede, function(x) {
+  meanAge = mean(x, na.rm = TRUE)
+  x[is.na(x)] = meanAge
+  return(x)
+})
+imputedAge = unlist(imputeAge)
+titanic$Age = imputedAge
+
 # Neue Variablen aus „Cabin“ 
   # Backbord (gerade Nummer), Steuerbord (ungerade Nummer)
   # Deck (vorangehender Buchstabe)  
@@ -18,5 +35,3 @@ titanic = read.csv("~/TU Dortmund/3. Semester/Wissenschaftliches Arbeiten/Gruppe
 # „PassengerID“, „Name“, „Ticket“ und „Cabin“ entfernen
 
 # R-Skript und neuen Datensatz in Repository abspeichern 
-
-# test hallo aktualisiert sich hier was
