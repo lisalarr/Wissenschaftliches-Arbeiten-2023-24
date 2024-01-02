1894a6d871ef4ee9baeb7a19eb079182c303865c
lisalarr <88594543+lisalarr@users.noreply.github.com>
2024-01-02 23:07:45 +0100

    Update Aufgabe2_1.R

--- Aufgabe2_1.R
+++ Aufgabe2_1.R
@@ -3,3 +3,46 @@
 # *              GitHub: Gruppe 12               *
 # *          Aufgabe 2 (Main Funktionen)         *
 # ************************************************
+
+# (i)
+# die Funktion berechnet deskriptive Statistiken (...) fuer metrische Variablen 
+# des Datensatzes 
+#
+# Input
+#
+# Output 
+
+# (ii) 
+# die Funktion berechnet deskriptive Statistiken (...) fuer kategoriale Variablen
+# des Datensatzes
+# 
+# Input
+#
+# Output
+
+# (iii)
+# die Funktion berechnet deskriptive bivariate Statistiken (...) fuer den Zusammenhang
+# kategorialer Variablen des Datensatzes
+#
+# Input
+#
+# Output
+
+# (iv)
+# die Funktion berechnet deskriptive bivariate Statistiken (...) fuer den Zusammenhang
+# einer metrischen und dichotomen Variable des Datensatzes
+#
+# Input
+#
+# Output
+
+# (v)
+# die Funktion erstellt eine Visualisierung (...) von drei/vier Variablen des 
+# Datensatzes
+#
+# Input
+#
+# Output
+
+# (vi)
+# ...  
