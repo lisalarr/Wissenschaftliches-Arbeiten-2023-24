831e3a9142615b19954225fef7e61a570c1ad77c
lisalarr <88594543+lisalarr@users.noreply.github.com>
2024-01-02 23:13:25 +0100

    Update Aufgabe2_1.R

--- Aufgabe2_1.R
+++ Aufgabe2_1.R
@@ -5,44 +5,73 @@
 # ************************************************
 
 # (i)
-# die Funktion berechnet deskriptive Statistiken (...) fuer metrische Variablen 
-# des Datensatzes 
+# metrStat - die Funktion berechnet deskriptive Statistiken fuer metrische 
+#            Variablen des Datensatzes. 
+#            Berechnet werden: ---
 #
 # Input
 #
 # Output 
 
+metrStat = function() {
+  
+}
+
 # (ii) 
-# die Funktion berechnet deskriptive Statistiken (...) fuer kategoriale Variablen
-# des Datensatzes
+# katStat - die Funktion berechnet deskriptive Statistiken fuer kategoriale 
+#           Variablen des Datensatzes.
+#           Berechnet werden: ---
+#
 # 
 # Input
 #
 # Output
 
+katStat = function() {
+  
+}
+
 # (iii)
-# die Funktion berechnet deskriptive bivariate Statistiken (...) fuer den Zusammenhang
-# kategorialer Variablen des Datensatzes
+# katBiStat - die Funktion berechnet deskriptive bivariate Statistiken fuer den 
+#             Zusammenhang kategorialer Variablen des Datensatzes.
+#             Berechnet werden: ---
+#
 #
 # Input
 #
 # Output
+
+katBiStat = function() {
+
+}
 
 # (iv)
-# die Funktion berechnet deskriptive bivariate Statistiken (...) fuer den Zusammenhang
-# einer metrischen und dichotomen Variable des Datensatzes
+# metrBiStat - die Funktion berechnet deskriptive bivariate Statistiken fuer den 
+#              Zusammenhang einer metrischen und dichotomen Variable des Daten-
+#              satzes.
+#              Berechnet werden: ---
+#
 #
 # Input
 #
 # Output
 
+metrBiStat = function() {
+  
+}
+
 # (v)
-# die Funktion erstellt eine Visualisierung (...) von drei/vier Variablen des 
-# Datensatzes
+# visualise - die Funktion erstellt eine Visualisierung von drei/vier Variablen des 
+#             Datensatzes. 
+#             Erstellt wird: --- 
 #
 # Input
 #
 # Output
+
+visualise = function() {
+  
+}
 
 # (vi)
 # ...  
