7c0288a86a1ebffe599e247cb914caeb795780aa
lisalarr <88594543+lisalarr@users.noreply.github.com>
2024-01-03 12:56:46 +0100

    Update Aufgabe1.R
    Anrede -> Title
    Factor korrigiert
    Bord/Deck umbenannt
    PassengerId zusätzlich entfernt

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -23,11 +23,12 @@ titanic$Pclass = ordered(as.factor(titanic$Pclass))
 
 # NA's von "Age" imputieren (Verfahren: arithmetisches Mittel)
 addAge = function(data) {
-  imputeAge = tapply(data$Age, data$Anrede, function(x) {
+  imputeAge = tapply(data$Age, data$Title, function(x) {
     meanAge = mean(x, na.rm = TRUE)
     x[is.na(x)] = meanAge
     return(x)
   })
+  
   imputedAge = unlist(imputeAge)
   data$Age = round(imputedAge)
   return(data)
@@ -35,25 +36,25 @@ addAge = function(data) {
 titanic = addAge(titanic)
 
 # Neue Variablen aus "Cabin" 
-# Backbord = 0, Steuerbord = 1, beides = 2
+# BackBord = 0, SteuerBord = 1, beides = 2
 variableCabin = function(data) {
   cabin = data$Cabin
   cabin[which(cabin == "")] = NA
   
-  bord = cabin
-  bord[which(nchar(bord) == 1)] = NA
-  bord[which(nchar(bord) == 2)] = substr(bord[which(nchar(bord) == 2)], 2, 2)
-  bord[which(nchar(bord) == 3)] = substr(bord[which(nchar(bord) == 3)], 2, 3)
-  bord[which(nchar(bord) == 4)] = substr(bord[which(nchar(bord) == 4)], 2, 4)
-  bord[which(nchar(bord) == 5)] = substr(bord[which(nchar(bord) == 5)], 4, 5)
-  bord[which(nchar(bord) > 5 & nchar(bord) < 15)] =
-    substr(bord[which(nchar(bord) > 5 & nchar(bord) < 15)], 2, 3)
-  bord[which(nchar(bord) >= 15)] = 1000
-  bord = as.integer(bord)
-  bord[which(bord != 1000)] = bord[which(bord != 1000)] %% 2
-  bord[which(bord == 1000)] = 2
+  Bord = cabin
+  Bord[which(nchar(Bord) == 1)] = NA
+  Bord[which(nchar(Bord) == 2)] = substr(Bord[which(nchar(Bord) == 2)], 2, 2)
+  Bord[which(nchar(Bord) == 3)] = substr(Bord[which(nchar(Bord) == 3)], 2, 3)
+  Bord[which(nchar(Bord) == 4)] = substr(Bord[which(nchar(Bord) == 4)], 2, 4)
+  Bord[which(nchar(Bord) == 5)] = substr(Bord[which(nchar(Bord) == 5)], 4, 5)
+  Bord[which(nchar(Bord) > 5 & nchar(Bord) < 15)] =
+    substr(Bord[which(nchar(Bord) > 5 & nchar(Bord) < 15)], 2, 3)
+  Bord[which(nchar(Bord) >= 15)] = 1000
+  Bord = as.integer(Bord)
+  Bord[which(Bord != 1000)] = Bord[which(Bord != 1000)] %% 2
+  Bord[which(Bord == 1000)] = 2
   
-  data = cbind(data, bord)
+  data = cbind(data, Bord)
   return(data)
 }
 titanic = variableCabin(titanic)
@@ -61,22 +62,22 @@ titanic = variableCabin(titanic)
 # Deck (vorangehender Buchstabe)
 # Mehrere Decks = 0, A = 1, B = 2, C = 2, ...
 variableDeck = function(data) {
-  deck = data$Cabin
-  deck[which(deck == "")] = NA
-  deck[which(nchar(deck) <= 4 | nchar(deck) > 5)] =
-    substr(deck[which(nchar(deck) <= 4 | nchar(deck) > 5)], 1, 1)
-  deck[which(nchar(deck) == 5)] = substr(deck[which(nchar(deck) == 5)], 1, 3)
+  Deck = data$Cabin
+  Deck[which(Deck == "")] = NA
+  Deck[which(nchar(Deck) <= 4 | nchar(Deck) > 5)] =
+    substr(Deck[which(nchar(Deck) <= 4 | nchar(Deck) > 5)], 1, 1)
+  Deck[which(nchar(Deck) == 5)] = substr(Deck[which(nchar(Deck) == 5)], 1, 3)
   
   for(i in 1:length(LETTERS)) {
-    deck[which(deck == LETTERS[i])] = i
+    Deck[which(Deck == LETTERS[i])] = i
   }
   
-  deck[which(nchar(deck) > 1)] = 0
-  deck = as.integer(deck)
-  data = cbind(data, deck)
+  Deck[which(nchar(Deck) > 1)] = 0
+  Deck = as.integer(Deck)
+  data = cbind(data, Deck)
   return(data)
 }
 titanic = variableDeck(titanic)
 
 # "PassengerID", "Name", "Ticket" und "Cabin" entfernen
-titanic[, c("PassengerID", "Name", "Ticket", "Cabin")] = list(NULL)
+titanic[ , c("PassengerId", "Name", "Ticket", "Cabin")] = list(NULL)
