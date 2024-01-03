da02b42be8a4af5748c664fd2242cfa9ded7a5a3
lisalarr <88594543+lisalarr@users.noreply.github.com>
2024-01-03 12:24:30 +0100

    Update Aufgabe1.R

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -4,22 +4,25 @@
 # *                  Aufgabe 1                   *
 # *        Antonia Koehr und Lisa Larrass        *
 # ************************************************
+
 titanic = read.csv("titanic.csv")
 
 # Anreden extrahieren
-titanic$Anrede = gsub("^.*,\\s*([A-Za-z]+)\\..*$", "\\1", titanic$Name)
+titanic$Title = gsub("^.*,\\s*([A-Za-z]+)\\..*$", "\\1", titanic$Name)
 
-# „Survived“, „Sex“, „Embarked“ zu factor umkodieren
+# Variablen zu Factor umkodieren
+# "Survived" mit ja = 1, nein = 2 
 titanic$Survived = as.factor(titanic$Survived)
-titanic$Sex = as.factor(titanic$Sex)
+# "Sex" mit male = 0, female = 1 
+titanic$Sex = as.factor(ifelse(titanic$Sex == "male", 0, 1))
+# "Embarked" mit Cherboourg = C, Queenstown = Q, Southampton = S 
 titanic$Embarked = as.factor(titanic$Embarked)
 
-# „Pclass“ als ordered-factor
+# "Pclass" als ordered-factor
 titanic$Pclass = ordered(as.factor(titanic$Pclass))
 
-# NA's von „Age“ imputieren (Verfahren: arithmetisches Mittel)
-# Gruppierung nach Anrede und Berechnung vom mean
-addAge <- function(data){
+# NA's von "Age" imputieren (Verfahren: arithmetisches Mittel)
+addAge = function(data) {
   imputeAge = tapply(data$Age, data$Anrede, function(x) {
     meanAge = mean(x, na.rm = TRUE)
     x[is.na(x)] = meanAge
@@ -29,53 +32,51 @@ addAge <- function(data){
   data$Age = round(imputedAge)
   return(data)
 }
-titanic <- addAge(titanic)
+titanic = addAge(titanic)
 
-
-# Neue Variablen aus „Cabin“ 
-# Backbord = 0; Steuerbord = 1; beides = 2
-variableCabin <- function(data){
-  cabin <- data$Cabin
-  cabin[which(cabin == "")] <- NA
-  bord <- cabin
-  bord[which(nchar(bord) == 1)] <- NA
-  bord[which(nchar(bord) == 2)] <- substr(bord[which(nchar(bord) == 2)], 2, 2)
-  bord[which(nchar(bord) == 3)] <- substr(bord[which(nchar(bord) == 3)], 2, 3)
-  bord[which(nchar(bord) == 4)] <- substr(bord[which(nchar(bord) == 4)], 2, 4)
-  bord[which(nchar(bord) == 5)] <- substr(bord[which(nchar(bord) == 5)], 4, 5)
-  bord[which(nchar(bord) > 5 & nchar(bord) < 15)] <- 
+# Neue Variablen aus "Cabin" 
+# Backbord = 0, Steuerbord = 1, beides = 2
+variableCabin = function(data) {
+  cabin = data$Cabin
+  cabin[which(cabin == "")] = NA
+  
+  bord = cabin
+  bord[which(nchar(bord) == 1)] = NA
+  bord[which(nchar(bord) == 2)] = substr(bord[which(nchar(bord) == 2)], 2, 2)
+  bord[which(nchar(bord) == 3)] = substr(bord[which(nchar(bord) == 3)], 2, 3)
+  bord[which(nchar(bord) == 4)] = substr(bord[which(nchar(bord) == 4)], 2, 4)
+  bord[which(nchar(bord) == 5)] = substr(bord[which(nchar(bord) == 5)], 4, 5)
+  bord[which(nchar(bord) > 5 & nchar(bord) < 15)] =
     substr(bord[which(nchar(bord) > 5 & nchar(bord) < 15)], 2, 3)
-  bord[which(nchar(bord) >= 15)] <- 1000
-  bord <- as.integer(bord)
-  bord[which(bord != 1000)] <- bord[which(bord != 1000)] %% 2
-  bord[which(bord == 1000)] <- 2
-  data <- cbind(data, bord)
+  bord[which(nchar(bord) >= 15)] = 1000
+  bord = as.integer(bord)
+  bord[which(bord != 1000)] = bord[which(bord != 1000)] %% 2
+  bord[which(bord == 1000)] = 2
+  
+  data = cbind(data, bord)
   return(data)
 }
-titanic <- variableCabin(titanic)
-
+titanic = variableCabin(titanic)
 
 # Deck (vorangehender Buchstabe)
-# mehrere Decks = 0, A = 1, B = 2, C = 2, ...
-variableDeck <- function(data){
-  deck <- data$Cabin
-  deck[which(deck == "")] <- NA
-  deck[which(nchar(deck) <= 4 | nchar(deck) > 5)] <- 
+# Mehrere Decks = 0, A = 1, B = 2, C = 2, ...
+variableDeck = function(data) {
+  deck = data$Cabin
+  deck[which(deck == "")] = NA
+  deck[which(nchar(deck) <= 4 | nchar(deck) > 5)] =
     substr(deck[which(nchar(deck) <= 4 | nchar(deck) > 5)], 1, 1)
-  deck[which(nchar(deck) == 5)] <- substr(deck[which(nchar(deck) == 5)], 1, 3)
+  deck[which(nchar(deck) == 5)] = substr(deck[which(nchar(deck) == 5)], 1, 3)
   
-  for(i in 1:length(LETTERS)){
-    deck[which(deck == LETTERS[i])] <- i
+  for(i in 1:length(LETTERS)) {
+    deck[which(deck == LETTERS[i])] = i
   }
-  deck[which(nchar(deck) > 1)] <- 0
-  deck <- as.integer(deck)
-  data <- cbind(data, deck)
+  
+  deck[which(nchar(deck) > 1)] = 0
+  deck = as.integer(deck)
+  data = cbind(data, deck)
   return(data)
 }
-titanic <- variableDeck(titanic)
+titanic = variableDeck(titanic)
 
-
-# „PassengerID“, „Name“, „Ticket“ und „Cabin“ entfernen
-titanic[, c("PassengerID", "Name", "Ticket", "Cabin")] <- list(NULL)
-
-# R-Skript und neuen Datensatz in Repository abspeichern 
+# "PassengerID", "Name", "Ticket" und "Cabin" entfernen
+titanic[, c("PassengerID", "Name", "Ticket", "Cabin")] = list(NULL)
