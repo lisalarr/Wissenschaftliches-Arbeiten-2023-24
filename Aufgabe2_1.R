99ef60b0e4c89c508f8b125a35a67ed109530018
fuwatson88 <153425834+fuwatson88@users.noreply.github.com>
2024-01-08 18:48:39 +0100

    Update Aufgabe2_1.R

--- Aufgabe2_1.R
+++ Aufgabe2_1.R
@@ -7,29 +7,60 @@
 # (i)
 # metrStat - die Funktion berechnet deskriptive Statistiken fuer metrische 
 #            Variablen des Datensatzes. 
-#            Berechnet werden: ---
+#            Berechnet werden die Lagemaße, einschließlich Mittelwert und 5
+#            -Punkte-Zusammenfassung; Streeungsmaße mit Varianz und
+#            Standardabweichung. Außerdem wurden die Schiefe und Wölbung der
+#            Verteilung berechnet. Zuletzt wurde die Anzahl der fehlenden Werte
+#            ausgegeben. 
 #
-# Input:
+# Input:     eine metrische Variable x und na.rm = TRUE als default
 #
-# Output:
+# Output:    Eine Liste mit den oben genannten deskriptiven Statistiken.
 
-metrStat = function() {
+metrStat = function(x, na.rm = TRUE){
+  if(is.numeric(x)){stats = list(Mittelwert = mean(x),
+                                 Varianz = var(x),
+                                 SD = sd(x),
+                                 Summary = summary(x),
+                                 NAs = sum(is.na(x)),
+                                 Schiefe =skewness(x),
+                                 Woelbung = kurtosis(x))
   
-}
+  return(stats)} 
+  else {print("Die Variable ist nicht metrisch.")}
+  }
+
+# Test
+#metrStat(titanic$Survived)
+#metrStat(titanic$Age)
 
 # (ii) 
 # katStat - die Funktion berechnet deskriptive Statistiken fuer kategoriale 
 #           Variablen des Datensatzes.
-#           Berechnet werden: ---
+#           Berechnet werden der Modus, die absolute und relative Häufigkeit.
+#           Bei der relativen Häufigkeit werden die Ergebnisse auf zwei
+#           Nachkommastellen aufgerundet. 
 #
-# 
-# Input:
+# Input: Eine kategoriale (factor oder character). 
 #
-# Output:
+# Output: Eine Liste mit den oben genannten deskriptiven Statistiken. 
 
-katStat = function() {
-  
+katStat = function(x) {
+  if(is.factor(x) || is.character(x)){
+    # Modalwert
+    stats = list(Modalwert = levels(x)[which.max(table(x))],
+                 Abs_Haufigkeit = table(x),
+                 Rel_Haufigkeit = round(100*prop.table(table(x)),2)
+    )
+    return(stats)
+    
+  }
+  else print("Die Variable ist nicht kategorial.")
 }
+
+# Test
+#katStat(titanic$Age)
+#katStat(titanic$Survived)
 
 # (iii)
 # katBiStat - die Funktion berechnet deskriptive bivariate Statistiken fuer den 
